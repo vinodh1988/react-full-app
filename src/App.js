ed4306c8c76349d6d2929774f98f5c50d511b3e6
vinodh1988 <k.c.vinodh@hotmail.com>
2022-06-09 10:14:48 +0530

    routing

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
 import './App.css'
+import {BrowserRouter as Router,Route,Link} from 'react-router-dom'
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
 
 const App = ()=>{
   return(
+<Router>
   <div>
-<div class="mt-4 p-5 bg-dark text-white rounded">
+<div className="mt-4 p-5 bg-dark text-white rounded">
   <h1>React World</h1>
   <p>Simple app using react</p>
 
   <ul id="menulist">
-       <li>Home</li> 
-       <li>About us</li>
-       <li>Contact us</li>
-
+       <li><Link to="/">Home</Link></li> 
+       <li><Link to="/about">About us</Link></li>
+       <li><Link to="/contact">Contact us</Link></li>
   </ul>
 </div>
+<div id="homearea">
+  
+
+   <Route path="/"  exact component={Home}></Route>
+   <Route path="/about"  component={About}></Route>
+   <Route path="/contact"  component={Contact}></Route>
+
+</div>
   </div>
+</Router>
   );
 }
 
