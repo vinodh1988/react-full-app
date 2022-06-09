2f16ab11149ca9ca64751e5578cd98f42599b618
vinodh1988 <k.c.vinodh@hotmail.com>
2022-06-09 09:31:22 +0530

    menu

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import './App.css'
 
 const App = ()=>{
   return(
@@ -5,6 +6,13 @@ const App = ()=>{
 <div class="mt-4 p-5 bg-dark text-white rounded">
   <h1>React World</h1>
   <p>Simple app using react</p>
+
+  <ul id="menulist">
+       <li>Home</li> 
+       <li>About us</li>
+       <li>Contact us</li>
+
+  </ul>
 </div>
   </div>
   );
